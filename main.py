1715119ed2bcd13cf9a975c96222b1d523076dac
tolu <toluadegbilero@gmail.com>
2023-09-23 23:38:15 -0400

    deletion

--- main.py
+++ main.py
@@ -109,18 +109,81 @@ elif st.session_state.expandtitle == "Unit 1.2":
 
 
 elif st.session_state.expandtitle == "Unit 1.3":
-    st.header("Unit 1.3: Variables")
-    st.markdown("Variables are values or characters that are stored within a phrase. "
-                "Some of the data types that variables can store are strings (any characters"
-                " placed within “ “), integers (any whole number), and floats (numbers with "
-                "decimal places).")
-    st.markdown("Making a variable and giving it a value is called variable "
-                "declaration. Some examples are shown below.")
-    st.code("A = 28 My_name = “John” \nNumber1 = 1 \n\t_car_ = “F-150” ", language='python')
-    st.markdown("Note the use of an equal sign to assign values to the variables."
-                " In Unit 3.1 we will go over Python’s equal symbol.")
-    st.markdown("By using variables we can label information using names that "
-                "allow code to be clear. ")
+    st.title("Variable Explanation App")
+
+    # Explanations
+    st.write("""
+    Variables are values or characters that are stored within a phrase. Some of the data types that variables can store are strings (any characters placed within “ “), integers (any whole number), and floats (numbers with decimal places).
+
+    Making a variable and giving it a value is called variable declaration. Some examples are shown below:
+    """)
+    st.code("A = 28\nMy_name = 'John'\nNumber1 = 1\n_car_ = 'F-150'", language='python')
+
+    st.write("""
+    Note the use of an equal sign to assign values to the variables. In Unit 3.1 we will go over Python’s equal symbol.
+
+    By using variables we can label information using names that allow code to be clear.
+    """)
+
+    # Code for a program
+    st.write("""
+    Below is the code for a program.
+    """)
+    st.code(
+        "import random\n"
+        "\n"
+        "num1 = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])\n"
+        "animal = random.choice(['Cow', 'Pig', 'Sheep', 'Dog', 'Whale'])\n"
+        "\n"
+        "your_random_password = str(animal) + str(num1)\n"
+        "print(your_random_password)",
+        language='python'
+    )
+
+    st.write("""
+    Even if you don’t understand some parts such as random.choice and str(), you can reasonably figure out that this is the code for a random password generator. As you progress through this course you will find out that it is not hard to get lost when overlooking code you are currently working on. By having appropriately named variables you will reduce the time you spend rereading your code to regain understanding of it.
+    """)
+
+    # Code with non-descriptive names
+    st.write("""
+    Below is the same code but with non-descriptive names.
+    """)
+    st.code(
+        "import random\n"
+        "\n"
+        "dwq = random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])\n"
+        "ejd = random.choice(['Cow', 'Pig', 'Sheep', 'Dog', 'Whale'])\n"
+        "\n"
+        "qua = str(ejd) + str(dwq)\n"
+        "print(qua)",
+        language='python'
+    )
+
+    st.write("""
+    As you can see, this code is much harder to understand compared to its descriptive version.
+    """)
+
+    # Variable naming rules
+    st.write("""
+    There are some rules tied with variable naming.
+    """)
+    st.write("**Variable Naming Rules:**")
+    st.write("- Variables must start with a letter or underscore.")
+    st.write("- Variables can only contain the letters/symbols A-Z, 0-9, and _.")
+    st.write("- Variables with different capitalization are different, even if the spelling is the same.")
+    st.write("- When making a variable that holds a word, ' ' must be used.")
+    st.write("- Variables cannot have the same name as keywords.")
+
+    # Examples of incorrect variable names
+    st.write("""
+    Some examples of incorrect variable names, along with reasoning as to why the code is incorrect.
+    """)
+    st.code("--hi = 3 #A non-letter/underscore character is at the front of the variable name"""", language='python')
+    st.code("", language='python')
+    st.code("", language='python')
+    st.code("", language='python')
+
+    st.write("" Making your variables descriptive is vital to creating readable code that is easily decipherable."")
 
 elif st.session_state.expandtitle == "Unit 1.4":
     st.header("Unit 1.4: Inputs and Outputs")
