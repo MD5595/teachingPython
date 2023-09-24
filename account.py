58103164714164d86e03d8d3748a1e20411188d2
rchand5043 <85791590+rchand5043@users.noreply.github.com>
2023-09-23 23:40:08 -0400

    Update account.py

--- account.py
+++ account.py
@@ -1,69 +1,45 @@
 import streamlit as st
 
-def account():
+def account_manager():
+    # Define a dictionary to store usernames and passwords
+    user_credentials = {
+        "user1": "password1",
+        "user2": "password2",
+        "user3": "password3"
+    }
 
-    user_credentials = {}
+    # Streamlit app title
+    st.title("Simple Username/Password Authentication")
 
-    st.title("Account")
+    # Streamlit sidebar for login and user creation
+    with st.sidebar:
+        st.header("Login")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
 
-    if st.button("Sign Up"):
-        create_credentials(user_credentials)
+        st.header("Create User")
+        new_username = st.text_input("New Username")
+        new_password = st.text_input("New Password", type="password")
+        create_user = st.button("Create User")
 
-    if st.button("Login"):
-
-        login()
-        user_info = login()
-        check_credentials(user_info[0], user_info[1], user_credentials)
-
-        #while flag:
-
-        if check_credentials(user_info[0], user_info[1], user_credentials):
-            st.success("Access granted")
-            #flag = False
+    # Streamlit main content
+    if st.sidebar.button("Login"):
+        if username in user_credentials and user_credentials[username] == password:
+            st.success("Access granted!")
         else:
-            st.error("Access denied, Try Again")
-            login()
+            st.error("Access denied. Please check your username and password.")
+
+    if create_user:
+        create_new_user(new_username, new_password, user_credentials)
+
+# Function to create a new user
+def create_new_user(username, password, user_credentials):
+    if username and password:
+        user_credentials[username] = password
+        st.success(f"User '{username}' has been created.")
+    else:
+        st.warning("Please provide both a username and a password for user creation.")
 
 
-def create_credentials(credentials):
 
-     #Asks user for their info
-    with st.form('Account Creation'):
-        username = (st.text_input("Enter a username for your account: "))
-        password = (st.text_input("Enter a password for your account (8 characters or numbers): ", type="password"))
-        st.form_submit_button("Sign Up!")
-
-     #stores user info in dictionary and returns dictionary
-    credentials[username] = password
-    return credentials
-
-
-def login():
-
-    cred_list = []
-    st.header('Log-In')
-    the_username = (st.text_input("Username: "))
-    the_password = (st.text_input("Password: ", type="password"))
-    st.button("Log In!")
-
-    cred_list.append(the_username)
-    cred_list.append(the_password)
-
-    return cred_list
-
-
-def check_credentials(username, password, credentials):
-    """
-    Using dictionary of account information, checks to see if user exists
-    when logging in
-    """
-
-    if username in credentials and (credentials[username] == password):
-        return True
-    elif (username in credentials) and (credentials[username] != password):
-        st.warning("Incorrect password, try again.")
-        return False
-    elif username not in credentials:
-        st.warning("Username incorrect, try again or Create Account.")
-        return False
 
