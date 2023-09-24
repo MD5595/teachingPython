58a62c471a257e425e249b333c74513530eb47b2
skiwojtek <skiwojtek@gmail.com>
2023-09-24 11:10:52 -0400

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -1125,7 +1125,7 @@ elif st.session_state.expandtitle == "Unit 3.4":
     st.write(
         "Understanding and mastering these conditional structures is essential for creating responsive and flexible programs.")
     if st.button("Next Section"):
-        st.session_state.expandtitle == "Unit 3.5"
+        st.session_state.expandtitle = "Unit 3.5"
 
 elif st.session_state.expandtitle == "Unit 3.5":
     st.header("Unit 3 Quiz")
